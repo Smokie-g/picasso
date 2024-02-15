a1a9e45360e79f0011f466429ec8da1e41d68c58
Андрей Голубчиков <andrejgolubcikov@MacBook-Pro-Andrej.local>
2024-02-15 21:20:42 +0300

    eslintrc file configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,59 @@
 module.exports = {
-  root: true,
-  extends: ["./.eslintrc.base.json", "plugin:jest/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "no-extra-boolean-cast": "off",
-    "no-console": "off",
-    "no-plusplus": "off",
-    "prettier/prettier": "error",
-    "no-void": ["error", { allowAsStatement: true }],
-    "no-shadow": "off",
+  plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks'],
+
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/typescript',
+  ],
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    sourceType: 'module',
+    ecmaFeatures: {
+      ecmaVersion: '2021',
+      sourceType: 'module',
+      jsx: true,
+    },
   },
+
+  env: {
+    browser: true,
+  },
+
+  rules: {
+    'no-extra-boolean-cast': 'off',
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'no-void': ['error', { allowAsStatement: true }],
+    'no-shadow': 'off',
+  },
+
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './',
+      },
+    },
+  },
+
   overrides: [
     {
-      files: ["**/*.{ts,tsx}"],
-      rules: {
-        "@typescript-eslint/no-unsafe-argument": "warn",
-        "@typescript-eslint/no-floating-promises": "off",
-        "@typescript-eslint/no-misused-promises": [
-          "error",
-          {
-            checksVoidReturn: {
-              arguments: false,
-              attributes: false,
-            },
-          },
-        ],
-        "@typescript-eslint/interface-name-prefix": 0,
-        "@typescript-eslint/no-empty-interface": 1,
-        "@typescript-eslint/unbound-method": 0,
-        "@typescript-eslint/no-unsafe-assignment": 0,
-        "@typescript-eslint/no-unsafe-member-access": 1,
-        "@typescript-eslint/restrict-template-expressions": 1,
-        "@typescript-eslint/no-unsafe-call": 1,
-        "@typescript-eslint/no-var-requires": 0,
-        "@typescript-eslint/ban-ts-comment": 1,
-        "@typescript-eslint/no-use-before-define": [
-          "error",
-          { variables: false },
-        ],
-        "react/no-unstable-nested-components": ["warn", { allowAsProps: true }],
+      files: ['!src/**'],
+      env: {
+        browser: false,
+        node: true,
       },
     },
   ],
-  env: {
-    jest: true,
-  },
-};
+}
